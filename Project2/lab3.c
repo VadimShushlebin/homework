c89588e0a4342f442b6101e848a58f40fd737551
Шушлебин Вадим <144512497+VadimShushlebin@users.noreply.github.com>
2025-10-01 18:53:52 +0300

    Add files via upload

---
+++ Project2/lab3.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+int main()
+{
+	float kat1,kat2;
+	setlocale(LC_ALL, "RUS");
+	puts("Введите длину первого катета:");
+	scanf("%f",&kat1);
+	puts("Введите длину второго катета:");
+	scanf("%f", &kat2);
+	float result = sqrt(pow(kat1, 2) + pow(kat2, 2));
+	printf("Гипотенуза прямоугольного треугольника равна %.2f", result);
+
+}
